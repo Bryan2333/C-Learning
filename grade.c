bb19bb51b6fde18f407b7b0483088fefc09baead
BryanLiang <updater1234@outlook.com>
2022-03-16 09:22:05 +0800

    grade

---
+++ grade.c
@@ -0,0 +1,15 @@
+// 判断一个分数是否及格
+#include <stdio.h>
+
+int main(void){
+    int grade;
+    printf("请输入你的分数：");
+    scanf("%d", &grade);
+    if(grade >= 60){
+        printf("你及格了\n");
+    }
+    else{
+        printf("你不及格\n");
+    }
+    return 0;
+}
