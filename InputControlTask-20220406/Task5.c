67bc49cafca23a9d65747591c3e95a2d614671cd
BryanLiang <hnnu.bryanliang2021@outlook.com>
2022-04-07 20:02:17 +0800

    update

---
+++ InputControlTask-20220406/Task5.c
@@ -0,0 +1,39 @@
+//хннЯ5
+#include <stdio.h>
+
+int main(void){
+	int guess, guesstimes, test, ifright = 0;
+	scanf("%d %d", &guess, &guesstimes);
+	for(int i = 1; i <= guesstimes; i++)
+	{
+		scanf("%d", &test);
+		if(test < 0){
+			break;
+		}
+		if(test > guess){
+			printf("Too big\n");
+		}
+		else if(test < guess){
+			printf("Too small\n");
+		}
+		else if(test == guess){
+			if(i == 1){
+				printf("Bingo!\n");
+				ifright = 1;
+			}
+			else if(i <= 3){
+				printf("Lucky You!\n");
+				ifright = 1;
+			}
+			else if(i > 3){
+				printf("Good Guess!\n");
+				ifright = 1;
+			}
+			break;
+		}
+	}
+	if(ifright == 0){
+		printf("Game Over\n");
+	}
+	return 0;
+}
