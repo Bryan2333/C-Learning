b43a0ecd30f79bb4a71866b8d999159711c41e9f
BryanLiang <202124120222@hainnu.edu.cn>
2022-05-29 18:06:27 +0800

    ADD "-"

--- SelfLearning/Function/FuncRandomNum1.c
+++ SelfLearning/Function/FuncRandomNum1.c
@@ -1,9 +1,17 @@
-//利用函数生成随机数字
+//利用rand函数生成随机数字
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(void)
 {
-        
+    srand(time(0));
+    int i;
+    for(i = 0; i < 5; ++i)
+    {
+        printf("%d\n", rand());
+    }
     return 0;
 }
